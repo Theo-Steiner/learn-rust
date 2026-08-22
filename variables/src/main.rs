b6e8ee091768fd456af0824ccc093c16bbe8285a
Theodor Steiner <theosteiner@gmail.com>
2026-08-22 13:38:47 +0900

    chore: start variables lesson

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+// actually computed at compile time => u32 10800
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    let x = THREE_HOURS_IN_SECONDS;
+    println!("The value of x is: {x}");
+    let x = x + 6;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}")
+    }
+    println!("The value of x is: {x}");
+}
