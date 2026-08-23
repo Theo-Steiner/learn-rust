6fb2a55f9a3394c5329dd4798779534b3b4a41b9
Theodor Steiner <theosteiner@gmail.com>
2026-08-23 23:41:37 +0900

    variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     let x = THREE_HOURS_IN_SECONDS;
     println!("The value of x is: {x}");
     let x = x + 6;
+    let spaces = "   ";
+    let spaces = spaces.len();
     {
         let x = x * 2;
         println!("The value of x in the inner scope is: {x}")
     }
-    println!("The value of x is: {x}");
+    println!("The value of x is: {x}, and there are {spaces} spaces");
 }
